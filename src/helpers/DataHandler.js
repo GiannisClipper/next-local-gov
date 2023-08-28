ead5ff595703815152f5a5de8f331952356b75c9
Giannis Clipper <giannisclipper@gmail.com>
2023-08-28 12:13:32 +0300

    Refactor DataHandler helper.

--- src/helpers/DataHandler.js
+++ src/helpers/DataHandler.js
@@ -1,11 +1,25 @@
 const fs = require( 'fs' );
 
+
 // gegneric parent class
 
-function Collection( csvFilename, topojsonFilename ) {
+function Collection( csvFilename, csvParser, topojsonFilename ) {
+
     this._csvFilename = csvFilename;
+    this._csvParser = csvParser;
     this._topojsonFilename = topojsonFilename;
+
     this._data = [];
+}
+
+Collection.prototype._read = function() {
+
+    let text = fs.readFileSync( this._csvFilename, "utf8" );
+    let rows = text.split( '\n' );
+    let result = rows.map( r => this._csvParser( r.split( ',' ) ) );
+
+    this._data = result;
+    return this;
 }
 
 Collection.prototype.findOne = function( func ) {
@@ -31,73 +45,62 @@ Collection.prototype.readTopojson = function() {
     return topojson;        
 }
 
+
 // specific child class
 
 function Periphereies( csvFilename, topojsonFilename ) {
-    Collection.call( this, csvFilename, topojsonFilename );
+
+    const csvParser = row => {
+        const [ id, name ] = row;
+        return { id, name }; 
+    }
+
+    Collection.call( this, csvFilename, csvParser, topojsonFilename );
+    this._read();
 }
 
 Periphereies.prototype = new Collection();
 
 Periphereies.prototype.constructor = Periphereies;
 
-Periphereies.prototype.read = function() {
-    const text = fs.readFileSync( this._csvFilename, "utf8" );
-    const rows = text.split( '\n' );
-    const result = rows.map( r => {
-        const [ id, name ] = r.split( ',' );
-        return { id, name }; 
-    } );
-
-    this._data = result;
-    return this;
-}
 
 // specific child class
 
 function Nomoi( csvFilename, topojsonFilename ) {
-    Collection.call( this, csvFilename, topojsonFilename );
+
+    const csvParser = row => {
+        const [ id, name, periph_name ] = row;
+        return { id, name, periph_name }; 
+    }
+
+    Collection.call( this, csvFilename, csvParser, topojsonFilename );
+    this._read();
 }
 
 Nomoi.prototype = new Collection();
 
 Nomoi.prototype.constructor = Nomoi;
 
-Nomoi.prototype.read = function() {
-    const text = fs.readFileSync( this._csvFilename, "utf8" );
-    const rows = text.split( '\n' );
-    const result = rows.map( r => {
-        const [ id, name, periph_name ] = r.split( ',' );
-        return { id, name, periph_name }; 
-    } )
-
-    this._data = result;
-    return this;
-}
 
 // specific child class
 
 function Dhmoi( csvFilename, topojsonFilename ) {
-    Collection.call( this, csvFilename, topojsonFilename );
+
+    const csvParser = row => {
+        let [ id, name, nomos_name, area, pop2021 ] = row;
+        area = parseFloat( area );
+        pop2021 = parseInt( pop2021 );
+        return { id, name, nomos_name, area, pop2021 }; 
+    }
+
+    Collection.call( this, csvFilename, csvParser, topojsonFilename );
+    this._read();
 }
 
 Dhmoi.prototype = new Collection();
 
 Dhmoi.prototype.constructor = Dhmoi;
 
-Dhmoi.prototype.read = function() {
-    const text = fs.readFileSync( this._csvFilename, "utf8" );
-    const rows = text.split( '\n' );
-    const result = rows.map( r => {
-        let [ id, name, nomos_name, area, pop2021 ] = r.split( ',' );
-        area = parseFloat( area );
-        pop2021 = parseInt( pop2021 );
-        return { id, name, nomos_name, area, pop2021 }; 
-    } )
-
-    this._data = result;
-    return this;
-}
 
 // specific class to export
 
@@ -117,10 +120,6 @@ function DataHandler() {
         "data/dhmoi_okxe_final.csv",
         "data/dhmoi_okxe_simplified.topojson"  
     );
-
-    this.periphereies.read();
-    this.nomoi.read();
-    this.dhmoi.read();
 }
 
 export default DataHandler;
