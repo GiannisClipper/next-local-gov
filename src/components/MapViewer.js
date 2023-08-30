6ebdf406ecb3e04571e4a0a32bd66904a8e19a84
Giannis Clipper <giannisclipper@gmail.com>
2023-08-30 12:24:32 +0300

    Refactor MapViewer component and add zoom feature.

--- src/components/MapViewer.js
+++ src/components/MapViewer.js
@@ -1,81 +1,63 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import * as d3 from 'd3';
 
 function MapViewer( { width, height, geojson, pathStroke, pathFill, textProp, onClickHandler } ) {
 
-    useEffect( () => {
-
-        // centering/scaling/translating map is based on `Jan van der Laan` answer/idea:
-        // https://stackoverflow.com/questions/14492284/center-a-map-in-d3-given-a-geojson-object
-
-        // a sample projection
-        const center = d3.geoCentroid( geojson ); // map coordinates
-        let scale  = 1;
-        let offset = [ width / 2, height / 2 ];
+  const [ zoom, setZoom ] = useState( 1000 );
   
-        let projection = d3.geoEquirectangular()
-            .center( center )
-            .scale( scale )
-            .translate( offset )
-
-        let geoPathGenerator = d3.geoPath().projection( projection );
-
-        // get the sample bounds and calculate values to scale and translate
-        let bounds = geoPathGenerator.bounds( geojson );
-        const [ sampleLeft, sampleTop ] = bounds[ 0 ];
-        const [ sampleRight, sampleBottom ] = bounds[ 1 ];
-        const sampleWidth = sampleRight - sampleLeft;
-        const sampleHeight = sampleBottom - sampleTop;
-
-        const hscale = scale * width / sampleWidth;
-        const vscale = scale * height / sampleHeight;
-        scale = ( hscale < vscale ) ? hscale : vscale;
-
-        const [ hOffset, vOffset ] = offset;
-        const hCorrection = ( hOffset - sampleWidth / 2 ) - sampleLeft;
-        const vCorrection = ( vOffset - sampleHeight / 2 ) - sampleTop;
-        offset = [
-            hOffset + hCorrection * scale,
-            vOffset + vCorrection * scale,
-        ];
-
-        // the actual projection
-        projection = d3.geoEquirectangular()
-            .center( center )
-            .scale( scale )
-            .translate( offset )
-
-        geoPathGenerator = geoPathGenerator.projection( projection );
+    useEffect( () => {
 
         const div = d3.select( `.map-content` );
 
-        const svg = div.select( "svg" )
-            // .attr( "width", width )
-            // .attr( "height", height )
+        const svg = div.append( "svg" )
+            .attr( "width", width )
+            .attr( "height", height )
             .attr( "stroke", "steelblue" )
             .attr( "fill", "white" );
 
-        const group = svg.append( "g" );
+        // const projection = d3.geoEquirectangular()
+        const projection = d3.geoMercator()
+            .scale( 1 )
+            .translate( [ 0, 0 ] );
+        
+        // Create a path generator.
+        const path = d3.geoPath().projection( projection );
+        
+        // Compute the bounds of a feature of interest, then derive scale & translate.
+        // based on https://stackoverflow.com/questions/14492284/center-a-map-in-d3-given-a-geojson-object
+        const [ [ left, top ],[ right, bottom ]] = path.bounds( geojson );
+        const scale = .95 / Math.max( ( right - left ) / width, ( bottom - top ) / height );
+        const translate = [ ( width - scale * ( right + left ) ) / 2, ( height - scale * ( bottom + top ) ) / 2 ];
+        
+        // Update the projection to use computed scale & translate.
+        projection
+            .scale( scale )
+            .translate( translate );
 
-        // group.append( "ellipse" )
+        const g = svg.append( "g" )
+           .attr( "class", "group features" )
+
+        // g.append( "ellipse" )
         //     .attr("cx", width / 2 )
         //     .attr("cy", height / 2 )
         //     .attr("rx", height/2 )
         //     .attr("ry", height/2)
         //     .attr("stroke", "yellow")
-        //     .attr( "fill", "transparent" );
+        //     .attr( "fill", "transparent" )
 
-        group.selectAll( "path" )
+        g.selectAll( "path" )
             .data( geojson.features )
             .enter()
             .append( "path" )
-            .attr( "d", geoPathGenerator )
+            .attr( "d", path )
             .style( 'stroke', pathStroke )
+            .style( "stroke-width", .9 )
             .style( 'fill', pathFill )
+
             .on( 'mouseover', function( e, d ) {
                 d3.select( this )
                     .transition()
-                    .duration( 200 )
+                    .duration( 400 )
                     .style( 'fill', 'lightsteelblue' )
             } )
             .on( 'mouseout', function( e, d ) { 
@@ -90,36 +72,46 @@ function MapViewer( { width, height, geojson, pathStroke, pathFill, textProp, on
                 }
             } )
 
-        // svg.append("rect")
-        //     .attr("x", bounds[ 0 ][ 0 ])
-        //     .attr("y", bounds[ 0 ][ 1 ])
-        //     .attr("width", bounds[ 1 ][ 0 ]-bounds[ 0 ][ 0 ])
-        //     .attr("height", bounds[ 1 ][ 1 ]-bounds[ 0 ][ 1 ])
-        //     .attr("stroke", "red");
-
-        group.selectAll( "text" )
+        g.selectAll( "text" )
+            .attr( "class", "svg-map-text" )
             .data( geojson.features )
             .enter()
             .append( "text" )
-            .attr( "transform", d => `translate(${geoPathGenerator.centroid( d )})` )
+            .attr( "transform", d => `translate(${path.centroid( d )})` )
             // .attr( "dy", ".35em" )
             .style( "text-anchor", "middle" )
-            .style( "font-size", ".66em" )
+            .style( "font-size", ".66rem" )
             .style( "color", "steelblue" )
+            .style( "stroke-width", .9 )
             .text( textProp );
+ 
+        const onZoomHandler = e => {
+            g.selectAll( "path" )
+                .style( "stroke-width", .9 / e.transform.k );
 
-        // const u = d3.select( `.map-content-${id}` )            
-        // .selectAll( 'path' )
-        // .data( featureCollection.features )
-        // .join( 'path' )
-        // .attr( 'd', geoGenerator );
+            g.selectAll( "text" )
+              .style( "font-size", `${ 2 * .66 / (1 + e.transform.k ) }rem` )
+              .style( "stroke-width", .9 / e.transform.k );
 
-    }, [ width, height ] );
+            g.attr( "transform", e.transform );
+        }
+
+        const zoom_behavior = d3.zoom()
+            .scaleExtent( [ 1, 10 ] )
+            // .translateExtent( [ 0, 0 ], [ width, height ] )
+            .on( 'zoom', onZoomHandler );
+
+        svg.call( zoom_behavior );
+
+    }, [] );
 
     return (
-        <div className={`map-content id`}>
-            <svg width={width} height={height}></svg>
+        <>
+        <div className={`map-content`}>
+            {/* <svg width={width} height={height}></svg> */}
         </div>
+        <div onClick={ e => { console.log( zoom ); setZoom( zoom + 1000 ); } }>zoom</div>
+        </>
     )
 }
 
